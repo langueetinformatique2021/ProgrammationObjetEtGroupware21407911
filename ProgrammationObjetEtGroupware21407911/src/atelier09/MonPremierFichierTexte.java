2ccd8c22786e643c1cf432073c2e2f43e24691fa
BELLAHBIB Ayoub <bellahbib.ayoub95@gmail.com>
2024-12-19 10:29:58 +0100

    Atelier09 - Exo1 Vérif

--- ProgrammationObjetEtGroupware21407911/src/atelier09/MonPremierFichierTexte.java
+++ ProgrammationObjetEtGroupware21407911/src/atelier09/MonPremierFichierTexte.java
@@ -7,16 +7,22 @@ import java.io.PrintWriter;
 
 public class MonPremierFichierTexte {
 	/** point d'entrée d'un exécutable */
+	static int num[] = { 21402871, 21104591, 21401764, 21408349, 21314076, 21109469, 21202064, 21415136, 21410005,
+			21415557, 21200439, 21407911, 21101218, 28607925, 21400908, 21314636 };
+	
 	public static void main(String[] args) {
-		int num[] = { 21402871, 21104591, 21401764, 21408349, 21314076, 21109469, 21202064, 21415136, 21410005,
-				21415557, 21200439, 21407911, 21101218, 28607925, 21400908, 21314636 };
 		try {
 			PrintWriter pr = new PrintWriter(new File("Nom.txt"));
 			for (int i = 0;i < num.length;i++) 
 				pr.println(num[i]);
 			pr.close();
-		} 
-		catch (IOException e) { e.printStackTrace(); }
+		} catch (IOException e) { e.printStackTrace();}
+		
+		vérifier();
+	}
+	
+	public static void vérifier() {
+		
 		String l;
 		int nL = 0;
 		boolean pareil = true;
@@ -30,21 +36,16 @@ public class MonPremierFichierTexte {
 					pareil = false;
 					break;
 				}
-				
 				nL++;
 			}
 			br.close();
-			
-			if (pareil == true) {
-				System.out.println("Le fichier contient bien les noms des membres du groupe");
-			}
-			else {
-				System.out.println("Attention, le fichier ne contient pas les noms des membres du groupe");
-			}
-			
-		} 
-		catch (IOException e) { e.printStackTrace(); }
+		}catch (IOException e) { e.printStackTrace(); }
 		
+		if (pareil == true) {
+			System.out.println("Le fichier contient bien les noms des membres du groupe");
+		}
+		else {
+			System.out.println("Attention, le fichier ne contient pas les noms des membres du groupe");
+		}
 	}
-
 }
